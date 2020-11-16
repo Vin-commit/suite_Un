8b2f3bc6ce0ed71e13a074c5f9fbfdadc6cce5fa
Vin-commit <74498336+Vin-commit@users.noreply.github.com>
2020-11-16 20:11:20 +0100

    Add files via upload

---
+++ suite_Un.py
@@ -0,0 +1,14 @@
+﻿#!/usr/bin/python3
+# coding:utf-8
+
+
+n = int(input("Calcul de Un, pour Un+1 = 6*Un + 2 et U0 = 8 avec n = "))
+i = 0
+cumul = u0 = 8
+while (i < n):
+  cumul = 6*cumul + 2
+  i += 1
+print ("U"+str(n)+" = "+str(cumul))
+
+
+-------------------------------------------------------------------------- Résultat ---------------------------------------------------------------------------
